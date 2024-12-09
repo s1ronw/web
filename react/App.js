399442cb0687fc3e5d512156814d7ed843f524f8
s1ronw <137923250+s1ronw@users.noreply.github.com>
2024-12-09 21:10:30 +0200

    Add files via upload

---
+++ react/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Board from './components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Memory Game</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
